8e323c80367ff595757d0ae06a752dca6732a648
Gluckz <anthany2024@gmail.com>
2026-06-23 22:12:39 -0600

    Disable legacy AdvancedRegionMarket integration

--- core/src/main/java/me/deadlight/ezchestshop/integrations/AdvancedRegionMarket.java
+++ core/src/main/java/me/deadlight/ezchestshop/integrations/AdvancedRegionMarket.java
@@ -1,40 +1,14 @@
 package me.deadlight.ezchestshop.integrations;
 
-import me.deadlight.ezchestshop.data.ShopContainer;
-import me.deadlight.ezchestshop.utils.objects.EzShop;
-import net.alex9849.arm.adapters.WGRegion;
-import net.alex9849.arm.events.RestoreRegionEvent;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import net.alex9849.arm.events.UnsellRegionEvent;
 
+/**
+ * Legacy AdvancedRegionMarket integration stub.
+ *
+ * PebbleShop is built for Pebble Quest/PebbleCore and no longer depends on
+ * AdvancedRegionMarket's external API at compile time. Keeping this class as a
+ * no-op preserves internal references without forcing CI to resolve the old ARM
+ * Maven artifact.
+ */
 public class AdvancedRegionMarket implements Listener {
-
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void handleRegionUnsell(UnsellRegionEvent event) {
-
-        WGRegion region = event.getRegion().getRegion();
-
-        for (EzShop shop : ShopContainer.getShops()) {
-            if (region.contains(shop.getLocation().getBlockX(), shop.getLocation().getBlockY(), shop.getLocation().getBlockZ())) {
-                ShopContainer.deleteShop(shop.getLocation());
-            }
-        }
-
-    }
-
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void handleRegionRestore(RestoreRegionEvent event) {
-
-        WGRegion region = event.getRegion().getRegion();
-
-        for (EzShop shop : ShopContainer.getShops()) {
-            if (region.contains(shop.getLocation().getBlockX(), shop.getLocation().getBlockY(), shop.getLocation().getBlockZ())) {
-                ShopContainer.deleteShop(shop.getLocation());
-            }
-        }
-
-    }
-
 }
