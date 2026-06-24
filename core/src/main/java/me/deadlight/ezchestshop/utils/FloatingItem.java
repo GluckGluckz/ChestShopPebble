363dc1bd09196034e839b9f978fa12c3c5a0247b
Gluckz <anthany2024@gmail.com>
2026-06-23 22:04:56 -0600

    Use shared Paper-safe version utilities for floating items

--- core/src/main/java/me/deadlight/ezchestshop/utils/FloatingItem.java
+++ core/src/main/java/me/deadlight/ezchestshop/utils/FloatingItem.java
@@ -1,11 +1,8 @@
 package me.deadlight.ezchestshop.utils;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-
-import java.util.logging.Level;
 
 public class FloatingItem {
 
@@ -13,44 +10,33 @@ public class FloatingItem {
     private Player player;
     private Location location;
 
-    static VersionUtils versionUtils;
-
-    static {
-        try {
-
-            if (Utils.isFolia()) {
-                versionUtils = (VersionUtils) Class.forName("me.deadlight.ezchestshop.utils.v1_20_R3").newInstance();
-
-            } else {
-                String packageName = Utils.class.getPackage().getName();
-                String internalsName = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
-                versionUtils = (VersionUtils) Class.forName(packageName + "." + internalsName).newInstance();
-            }
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | ClassCastException exception) {
-            Bukkit.getLogger().log(Level.SEVERE, "EzChestShop could not find a valid implementation for this server version.");
-        }
-    }
-
     public FloatingItem(Player player, ItemStack itemStack, Location location) {
 
         this.player = player;
         this.entityID = (int) (Math.random() * Integer.MAX_VALUE);
         this.location = location;
-        versionUtils.spawnFloatingItem(player, location, itemStack, entityID);
+        getVersionUtils().spawnFloatingItem(player, location, itemStack, entityID);
 
     }
 
     public void destroy() {
-        versionUtils.destroyEntity(player, entityID);
+        getVersionUtils().destroyEntity(player, entityID);
     }
 
     public void teleport(Location location) {
         this.location = location;
-        versionUtils.teleportEntity(player, entityID, location);
+        getVersionUtils().teleportEntity(player, entityID, location);
     }
 
     public Location getLocation() {
         return location;
+    }
+
+    private VersionUtils getVersionUtils() {
+        if (Utils.versionUtils == null) {
+            Utils.versionUtils = new PaperVersionUtils();
+        }
+        return Utils.versionUtils;
     }
 
 }
